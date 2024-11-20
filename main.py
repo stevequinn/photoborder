0e2aa8c00b5da673f74a5a616a456b8f02ae9cdc
Anthony <19926955+Rustymage@users.noreply.github.com>
2024-11-20 08:13:16 +0000

    Logging Update
    Removed milli seconds and function name from logging to improve readability

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from text import validate_font
 
 # Enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger(__name__)
 
